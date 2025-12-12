f9e66c8507beff2c6d974c31646be9c457141103
Ender <203009739+i-am-ender@users.noreply.github.com>
2025-12-12 14:40:59 -0300

    File pass the first 4 tests

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,7 +9,9 @@
 
 // Add your code right below, good luck!
 
+let firstName = "Carlos", lastName = "Stevenson", thisYear = 1965, birthYear = 1947
 
+console.log = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`
 
 
 
